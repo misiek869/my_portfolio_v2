60097b65002102d61cb8323fa97d8c4f4fc381b8
Michal Gawik <gawlik.zator@o2.pl>
2025-04-28 13:45:39 +0200

    add btn translation

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -46,7 +46,7 @@ const ProjectCard = ({
 			<Link
 				to={`/projects/${id}`}
 				className='inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none mt-6'>
-				Dowiedz się więcej
+				{t('moreBtn')}
 			</Link>
 		</article>
 	)
