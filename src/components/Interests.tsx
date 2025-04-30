3204276e75f848b20386d8866b50813f4fa13f4a
Michal Gawik <gawlik.zator@o2.pl>
2025-04-30 05:35:45 +0200

    create component

---
+++ src/components/Interests.tsx
@@ -0,0 +1,21 @@
+import { useTranslation } from 'react-i18next'
+
+const Interests = () => {
+	const { t } = useTranslation()
+
+	return (
+		<section className='mt-20 p-10 bg-[#DDDDDD] rounded-sm'>
+			<h2 className='bg-gradient-to-r to-red-950 via-gray-900 from-purple-950 inline-block text-transparent bg-clip-text text-4xl font-bold tracking-wide capitalize'>
+				{t('aboutTitle')}
+			</h2>
+			<p className='text-xl tracking-wide leading-loose md:text-2xl md:leading-relaxed'>
+				{t('about_01')}
+			</p>
+			<p className='text-xl tracking-wide leading-loose md:text-2xl mt-6 md:leading-relaxed'>
+				{t('about_02')}
+			</p>
+		</section>
+	)
+}
+
+export default Interests
