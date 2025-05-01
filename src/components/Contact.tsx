006295cbf4245d6f3644c5b2fec21cd4bf5c92ec
Michal Gawik <gawlik.zator@o2.pl>
2025-05-01 18:49:29 +0200

    add content

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 const Contact = () => {
-	return <div></div>
+	const { t } = useTranslation()
+	return (
+		<section
+			style={{
+				background: `
+  radial-gradient(circle at 30% -30%, #ee79de 0%, transparent 48%),
+  radial-gradient(circle at 70% 150%, #ee79de 0%, #000 48%)
+`,
+			}}
+			className='h-screen mt-20 p-10 rounded-sm flex flex-col justify-center'>
+			<h2 className='text-white text-8xl font-bold tracking-wide capitalize mb-10'>
+				{t('contactTitle')}
+			</h2>
+			<h3 className='text-white text-8xl font-bold tracking-wide mb-6'>
+				gawlik.zator@o2.pl
+			</h3>
+			<h3 className='text-white text-8xl font-bold tracking-wide '>
+				+48 512-374-866
+			</h3>
+		</section>
+	)
 }
 
 export default Contact
